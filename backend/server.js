130f3eb7f61ae5041bc9926bf3b3de2cb8876976
Musa Khan <16khanmusa@gmail.com>
2025-08-09 17:04:29 +0530

    Clean up repo: remove node_modules, env, dist

--- backend/server.js
+++ backend/server.js
@@ -29,11 +29,12 @@ app.use("/api/apply",applicationRoutes)
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/dist")));
+ app.use(express.static(path.join(__dirname1, "../frontend/dist")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"))
-  );
+ app.get("*", (req, res) =>
+   res.sendFile(path.resolve(__dirname1, "../frontend/dist", "index.html"))
+ );
+
 } else {
   app.get("/", (req, res) => {
     res.send("Api is Running");
